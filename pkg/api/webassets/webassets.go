c6fac4ce955d5597443b1ba21f53ad090252c072
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-08-20 18:35:48 -0300

    WebAssets: improve checks and error messages on dtos (#92093)
    fix web assets check and improve error message

--- pkg/api/webassets/webassets.go
+++ pkg/api/webassets/webassets.go
@@ -122,13 +122,16 @@ func readWebAssets(r io.Reader) (*dtos.EntryPointAssets, error) {
 	}
 
 	if entryPoints.App == nil || len(entryPoints.App.Assets.JS) == 0 {
-		return nil, fmt.Errorf("missing app entry")
+		return nil, fmt.Errorf("missing app entry, try running `yarn build`")
 	}
 	if entryPoints.Dark == nil || len(entryPoints.Dark.Assets.CSS) == 0 {
-		return nil, fmt.Errorf("missing dark entry")
+		return nil, fmt.Errorf("missing dark entry, try running `yarn build`")
 	}
 	if entryPoints.Light == nil || len(entryPoints.Light.Assets.CSS) == 0 {
-		return nil, fmt.Errorf("missing light entry")
+		return nil, fmt.Errorf("missing light entry, try running `yarn build`")
+	}
+	if entryPoints.Swagger == nil || len(entryPoints.Swagger.Assets.JS) == 0 {
+		return nil, fmt.Errorf("missing swagger entry, try running `yarn build`")
 	}
 	if entryPoints.Merqury == nil || len(entryPoints.Merqury.Assets.CSS) == 0 {
 		return nil, fmt.Errorf("missing merqury entry")
